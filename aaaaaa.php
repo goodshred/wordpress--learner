c700ba6045f32ae78f1992889d91d25c71e83372
goodshred <34416666+goodshred@users.noreply.github.com>
2018-04-24 21:19:43 +0800

    Initial commit

---
+++ aaaaaa.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set('Asia/Shanghai');
+?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>欢迎来到主页</title>
+</head>
+<body>
+<h1 
+align="center">我的主页</h1>
+当前时间：<?php echo(date('Y-m-d H:i:s'));?><br>
+星期<?php echo(date('N'));?>
+<p 
+align="center">欢迎大家访问我的网站，希望大家给我提出更多的意见。
+</body>
+</html>
